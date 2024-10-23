496165fa3bf88a7b4eed6113d14bd43d65b992fc
Rushi <kolperushikesh1121@gmail.com>
2024-10-23 23:30:17 +0530

    new 105

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello form 103 lab");
+        System.out.println("hello form 105 lab");
     }
 }
